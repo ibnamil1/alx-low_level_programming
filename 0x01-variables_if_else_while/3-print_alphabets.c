a71eef1895932291a3d7af825f82a747ac378e36
Liman <limanibrahim@gmail.com>
2022-09-09 17:02:36 -0700

    c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,20 +10,15 @@ int main(void)
 {
 	char i;
 	char j;
+
 	for (i = 'a'; i <= 'z'; i++)
 	{
-		putchar(i);							}
-
-					for (j = 'A'; j <= 'Z'; j++)
-
-							{
-
-										putchar(j);
-
-											}
-
-						putchar('\n');
-
-							return (0);
-
+		putchar(i);
+	}
+	for (j = 'A'; j <= 'Z'; j++)
+	{
+		putchar(j);
+	}
+	putchar('\n');
+	return (0);
 }
