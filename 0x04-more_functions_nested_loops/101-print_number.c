2b404702860232b4c7c7a77cffd1af85f5ec4bee
Liman <limanibrahim@gmail.com>
2022-09-15 17:04:27 -0700

    101-print_number

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_number - prints a number using _putchar
+ * @n: number to print
+ *
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int un = 0;
+
+	if (n < 0)
+	{
+		if (n < 1000000000)
+		{
+			n = -n
+		}
+		un = n;
+		_putchar(45);
+		num_to_char(n);
+	}
+	else
+	{
+		un = n;
+		num_to_char(un);
+	}
+}
