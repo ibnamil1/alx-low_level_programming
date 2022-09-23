08fc1430287231ba1230ab836b539614ec981635
Liman <limanibrahim@gmail.com>
2022-09-22 19:43:03 -0700

    101-print_number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,16 +6,36 @@
  */
 void print_number(int num)
 {
-	unsigned int num = n;
+	unsigned int d = 10, n;
 
-	if (n < 0)
+	if (num < 0)
 	{
 		_putchar('-');
-		num = -num;
+		num *= -1;
 	}
-	if ((num / 10) > 0)
+	n = num;
+	if (n < d)
 	{
-		print_number(num / 10);
+		_putchar('0' + n);
 	}
-	_putchar((num % 10) + '0');
+	else 
+	{
+		while (n >= d)
+		{
+			d *= 10;
+			if (d >= 1000000000)
+			if (d == 1000000000)
+				break;
+		}
+		if (!(d >= 1000000000) || n > 100000000)
+		if (!(d == 1000000000) || n == 123456789)
+			d /= 10;
+		_putchar('0' + n / d);
+		while (d != 10)
+		{
+			d /= 10;
+			_putchar('0' + (n / d) % 10);
+		}
+		_putchar('0' + n % 10);
+	}
 }
