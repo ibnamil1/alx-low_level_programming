76637b9fd876576da609b60d850b9e0f5b3dacb2
Liman <limanibrahim@gmail.com>
2022-09-28 10:16:30 -0700

    0-puts_recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,17 +2,18 @@
 
 /**
  * _puts_recursion - prints a string
- * @s: string 
+ *
+ * @s: string
  */
 void _puts_recursion(char *s)
 {
-	if (*s == '\n')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 	}
 	else
 	{
-	_putchar(*s);
-	_puts_recursion(s + 1);
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
 }
