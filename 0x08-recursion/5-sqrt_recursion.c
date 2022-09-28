39dacefbb6208b13b5fad10ec878f1c9926d37f1
Liman <limanibrahim@gmail.com>
2022-09-28 11:26:53 -0700

    5-sqrt_recursion

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -19,3 +19,25 @@ int _sqrt_recursion(int n)
 	}
 	return (is_sqrt(n, square));
 }
+/**
+ * is_sqrt - function to check whether it's a natural square root or not
+ * @n: number
+ * @square: test number
+ * Return: int
+ */
+int is_sqrt(int n, int square)
+{
+	if (square * square == n)
+	{
+		return (square);
+	}
+	else if (square * square < n)
+	{
+		return (is_sqrt(n, square + 1));
+	}
+	else if (square * square > n)
+	{
+		return (-1);
+	}
+	return (-1);
+}
