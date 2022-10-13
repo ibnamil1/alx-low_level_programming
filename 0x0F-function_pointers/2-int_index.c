77e1240e800d45d112f4e5dcbbdab9e7bbf0c6d4
Liman <limanibrahim@gmail.com>
2022-10-13 12:54:36 -0700

    2-int_index

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,14 +12,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 	int i;
 
 	if (array && cmp && size > 0)
-	{
 		for (i = 0; i < size; i++)
-		{
 			if (cmp(array[i]))
-			{
 				return (i);
-			}
-		}
-	}
 	return (-1);
 }
