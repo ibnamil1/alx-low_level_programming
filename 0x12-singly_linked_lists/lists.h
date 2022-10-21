3cac8be2cdd6c6f0e19cbcd32616498fed97130b
Liman <limanibrahim@gmail.com>
2022-10-20 18:13:46 -0700

    0-print_list

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,10 +1,14 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_
+#define _LISTS_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /**
- * struct list_s - singly linked lists
+ * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
- * @len: length of string
+ * @len: length of the string
  * @next: points to the next node
  * Description: singly linked list node structure
  */
@@ -13,7 +17,7 @@ typedef struct list_s
 	char *str;
 	unsigned int len;
 	struct list_s *next;
-}list_t;
+} list_t;
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
